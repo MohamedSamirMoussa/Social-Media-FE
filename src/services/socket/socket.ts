370dda282f615e2a63fcf949ed850ddd54cf0d2d
Mohamed Samir <mosamir5000@gmail.com>
2026-09-04 20:50:07 +0300

    fixxxxx

--- src/services/socket/socket.ts
+++ src/services/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
 export const socket: Socket = io(import.meta.env.VITE_BE_SOCKET_URI, {
-  autoConnect: false,
+  transports: ["websocket"],
   withCredentials: true,
 });
